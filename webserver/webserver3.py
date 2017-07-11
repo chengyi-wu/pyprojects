e85b01827b172e93b8a7aa8f6af0c0f38c5f542e
chenwu <chengyi.wu@outlook.com>
2017-07-11 10:23:48 +0800

    +ex3
    Signal

--- webserver/webserver3.py
+++ webserver/webserver3.py
@@ -1,12 +1,21 @@
 import os
 import socket
 import time
+import signal
+import errno
 
 SERVER_ADDRESS = (HOST, PORT) = ('', 8888)
 REQUEST_QUEUE_SIZE = 5
 
+def grim_reaper(signum, frame):
+    pid, status = os.wait()
+    print(
+        'Child {pid} terminated with status {status}\n'.format(pid=pid, status=status)
+    )
+
 def handle_request(client_connection):
-    requst = client_connection.recv(1024)
+    request = client_connection.recv(1024)
+    print(request.decode())
     http_response = '''\
     HTTP/1.1 200 OK
 
@@ -14,8 +23,8 @@ def handle_request(client_connection):
     '''
 
     client_connection.sendall(http_response)
-    print('DEBUG\tSlepping for 60s...')
-    time.sleep(60)
+    print('DEBUG\tSlepping for 6s...')
+    time.sleep(6)
 
 def serve_forever():
     listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,9 +33,17 @@ def serve_forever():
     listen_socket.listen(REQUEST_QUEUE_SIZE)
     print('Serving HTTP on port {port} ...'.format(port=PORT))
 
-    #clients = []
+    signal.signal(signal.SIGCHLD, grim_reaper)
+    
     while True:
-        client_connection, client_address = listen_socket.accept()
+        try:
+            client_connection, client_address = listen_socket.accept()
+        except IOError as e:
+            code, msg = e.args
+            if code == errno.EINTR: # Raised by the exit of the child process
+                continue
+            else:
+                raise
         # fork() deosn't work for Windows...
         pid = os.fork()
         if pid == 0: # child
